ac145e819dd1515ab78e6a1f83cc29e147539325
Mykaelsss <mykaelsicard04@gmail.com>
2024-02-05 20:52:39 -0500

    remove folder

--- components/formResults/UsernameFormResult.tsx
+++
@@ -1,9 +0,0 @@
-import validUsername from "@/functions/validUsername"
-
-export default function UsernameFormResult() {
-    return (
-        <div className='bg-red-400 border border-red-400 p-2 text-center rounded-md'>
-            Taken username
-        </div>
-    )
-}
